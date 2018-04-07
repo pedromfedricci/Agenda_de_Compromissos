d2d85b62aabdb344c57f2c0263acc5f8f42aa8ed
pedroschiavom <31569709+pedroschiavom@users.noreply.github.com>
2018-04-06 22:09:01 -0300

    Add files via upload
    atributo e método senha

--- headers/Usuario.h
+++ headers/Usuario.h
@@ -1,9 +1,12 @@
+//
+// Created by pedro on 21/03/18.
+//
+
 #ifndef USUARIO_H
 #define USUARIO_H
 
 #include "DoublyLinkedList.h"
 #include "Compromisso.h"
-#include "../json/json.h"
 #include <iostream>
 using namespace std;
 
@@ -13,12 +16,15 @@ public:
     DoublyLinkedList<Compromisso> compromissos;
 
     Usuario();
-    Usuario(const string&, const string&);
+    Usuario(const string&, const string&, const string&);
     ~Usuario();
     string getUsername() const;
     void setUsername(const string &username);
     string getNome() const;
     void setNome(const string &nome);
+    string getPassword()const;
+    void setPassword(const string &password);
+
 
     bool operator==(const Usuario &);
     bool operator!=(const Usuario &);
@@ -26,15 +32,15 @@ public:
     bool operator>(const Usuario &);
     bool operator<=(const Usuario &);
     bool operator<(const Usuario &);
+
     Usuario& operator=(const Usuario &);
     friend ostream& operator<<(ostream &, const Usuario &);
-
-    Json::Value toJson();
-    void fromJson(Json::Value);
 
 private:
     string username;
     string nome;
+    string password;
 };
+
 
 #endif //USUARIO_H
