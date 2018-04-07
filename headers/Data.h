a2122cf91a90946ee656b8ace9d1fd8c3611ea14
LincolnDias02 <32851233+LincolnDias02@users.noreply.github.com>
2018-04-07 19:47:55 -0300

    Add files via upload

---
+++ headers/Data.h
@@ -0,0 +1,48 @@
+#ifndef DATA_H
+#define DATA_H
+
+#include <iostream>
+#include "json.h"
+using namespace std;
+
+class Data {
+
+public:
+    Data();
+    Data(int,int,int);
+    Data(int, int);
+    explicit Data(int);
+
+    void setData(int,int,int);
+    void setAno(int);
+    int getAno() const;
+    string getAnoString() const;
+    void setMes(int);
+    int getMes() const;
+    string getMesString() const;
+    void setDia(int);
+    int getDia() const;
+    string getDiaString() const;
+
+    bool operator==(const Data &);
+    bool operator!=(const Data &);
+    bool operator>=(const Data &);
+    bool operator>(const Data &);
+    bool operator<=(const Data &);
+    bool operator<(const Data &);
+    Data& operator=(const Data &);
+    friend ostream& operator<<(ostream &, const Data &);
+
+    Json::Value toJson();
+    void fromJson(Json::Value);
+
+    static const int diasPorMes[];
+    static const string nomeMes[];
+
+private:
+    int mes;
+    int dia;
+    int ano;
+};
+
+#endif //DATA_H
