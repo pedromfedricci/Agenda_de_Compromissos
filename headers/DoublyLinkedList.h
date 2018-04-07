26be1bc7bf21aa31aa02ef6c15a2e45f5109fd62
Pedro de Matos Fedricci <pedromfedricci@gmail.com>
2018-04-07 17:27:12 -0300

    Add files via upload

--- headers/DoublyLinkedList.h
+++ headers/DoublyLinkedList.h
@@ -12,7 +12,7 @@ using namespace std;
 template<class T>
 class DoublyLinkedList;
 template<class T>
-ostream& operator<<(ostream&, const DoublyLinkedList<T> &);
+ostream& operator<< (ostream&, const DoublyLinkedList<T> &);
 
 /**
  * Classe representa uma lista dinâmica duplamente
@@ -36,18 +36,26 @@ public:
     ~DoublyLinkedList();
     void insertInOrder(T &);
     void printList(const string&, const string&);
-    Node<T>* linearSearch(T &);
     bool removeOne(T &);
+    void deleteAll();
     bool iterate(T &, long);
+    bool stepperForward(T &, bool b = true);
+    bool stepperContent(T &);
+    void setStepperToStart(bool b = true);
 
-    DoublyLinkedList<T>&operator=(const DoublyLinkedList<T>&);
+    DoublyLinkedList<T>& operator+=(const DoublyLinkedList<T>&);
+    DoublyLinkedList<T>& operator=(const DoublyLinkedList<T>&);
+
     friend ostream& operator<< <T>(ostream &os, const DoublyLinkedList<T> &list);
 
 private:
     long size;
-    Node<T>* tail;
-    Node<T>* header;
+    Node<T> *tail;
+    Node<T> *header;
+    Node<T> *stepper;
+
     void setSize(long);
+    Node<T>* linearSearch(T &);
 };
 
 /**
@@ -60,6 +68,7 @@ DoublyLinkedList<T>::DoublyLinkedList()
     DoublyLinkedList::size = 0;
     DoublyLinkedList::header = nullptr;
     DoublyLinkedList::tail = nullptr;
+    DoublyLinkedList::stepper = nullptr;
 }
 
 /**
@@ -107,7 +116,7 @@ void DoublyLinkedList<T>::insertInOrder(T &newContent)
         previous->setNext(node);
         node->setPrevious(previous);
     }
-    else header = node;
+    else stepper = header = node;
 
     if (current)
         current->setPrevious(node);
@@ -138,11 +147,11 @@ void DoublyLinkedList<T>::printList(const string &txt,const string &format)
 }
 
 /**
- * Destrutor desaloca os Node<T> da lista
+ * Desaloca todos Nodes da Lista
  * @tparam T template de objeto genérico 'T'
  */
 template<class T>
-DoublyLinkedList<T>::~DoublyLinkedList()
+void DoublyLinkedList<T>::deleteAll()
 {
     Node<T> *current = header;
     Node<T> *previous = nullptr;
@@ -155,6 +164,16 @@ DoublyLinkedList<T>::~DoublyLinkedList()
         size--;
     }
     //cout << "\n" << "Nodes nao desalocados: " << size << endl;
+}
+
+/**
+ * Destrutor desaloca os Node<T> da lista
+ * @tparam T template de objeto genérico 'T'
+ */
+template<class T>
+DoublyLinkedList<T>::~DoublyLinkedList()
+{
+    DoublyLinkedList::deleteAll();
 }
 
 /**
@@ -195,22 +214,50 @@ bool DoublyLinkedList<T>::removeOne(T &matchContent)
 
     if(!nodeMatch) return false;
 
-    if (nodeMatch == tail)
-        tail = nodeMatch->getPrevious();
-
     if (nodeMatch->getPrevious())
         nodeMatch->getPrevious()->setNext(nodeMatch->getNext());
 
     else header = nodeMatch->getNext();
 
+    //possível reposição de tail
+    if (nodeMatch == tail)
+        tail = nodeMatch->getPrevious();
+
+    //possível reposição de stepper
+    if (stepper == nodeMatch)
+    {
+        if (nodeMatch->getPrevious())
+            stepper = nodeMatch->getPrevious();
+        else stepper = nodeMatch->getNext();
+    }
+
     delete(nodeMatch);
     size--;
-
     return true;
 }
 
 /**
- * Sobrecarga aloca uma nova série de Node<T>
+ * Sobrecarga adiciona uma nova série de Node<T>
+ * para uma lista, carregando o conteúdo de outra lista
+ * @tparam T template de objeto genérico 'T'
+ * @param list lista da classe 'DoublyLinkedList'
+ * @return objeto 'DoublyLinkedList' resultado da operação
+ */
+template<class T>
+DoublyLinkedList<T>& DoublyLinkedList<T>::operator+=(const DoublyLinkedList<T> &list)
+{
+    Node<T> *current = list.header;
+    while (current)
+    {
+        DoublyLinkedList::insertInOrder(current->getContent());
+        current = current->getNext();
+    }
+
+    return *this;
+}
+
+/**
+ * Sobrecarga sobrepõe uma nova série de Node<T>
  * para uma lista, carregando o conteúdo de outra lista
  * @tparam T template de objeto genérico 'T'
  * @param list lista da classe 'DoublyLinkedList'
@@ -219,20 +266,8 @@ bool DoublyLinkedList<T>::removeOne(T &matchContent)
 template<class T>
 DoublyLinkedList<T>& DoublyLinkedList<T>::operator=(const DoublyLinkedList<T> &list)
 {
-    if (!list.header)
-    {
-        DoublyLinkedList::header = nullptr;
-        DoublyLinkedList::tail = nullptr;
-    }
-    else
-    {
-        Node<T> *current = list.header;
-        while (current)
-        {
-            DoublyLinkedList::insertInOrder(current->getContent());
-            current = current->getNext();
-        }
-    }
+    DoublyLinkedList::deleteAll();
+    *this += list;
     return *this;
 }
 
@@ -260,10 +295,10 @@ ostream& operator<<(ostream &os, const DoublyLinkedList<T> &list)
 
 /**
  *
- * @tparam T
- * @param outContent
- * @param index
- * @return
+ * @tparam T template de objeto genérico 'T'
+ * @param outContent conteúdo em certa posição
+ * @param qnt número de posições a serem percorridas
+ * @return bool determina se outContent é coerente
  */
 template<class T>
 bool DoublyLinkedList<T>::iterate(T & outContent, long qnt)
@@ -278,6 +313,63 @@ bool DoublyLinkedList<T>::iterate(T & outContent, long qnt)
 
     outContent = current->getContent();
 
+    return true;
+}
+
+/**
+ * Move o apontamento de stepper para o
+ * imediatamente próximo, ou para o
+ * emediatamente anterior
+ * @tparam T template de objeto genérico 'T'
+ * @param outContent conteúdo apontado por stepper
+ * @param b true stepper aponta para próximo, false,
+ * para o anterior
+ * @return bool determina se outContent é coerente
+ */
+template<class T>
+bool DoublyLinkedList<T>::stepperForward(T &outContent, bool b)
+{
+    if (!header) return false;
+
+    if (b && stepper->getNext())
+        stepper = stepper->getNext();
+
+    else if (!b && stepper->getPrevious())
+        stepper = stepper->getPrevious();
+
+    outContent = stepper->getContent();
+
+    return true;
+}
+
+/**
+ * Seta o apontamento de stepper para começo ou fim
+ * @param b true stepper aponta para começo da lista,
+ * false, para o fim
+ * @tparam T template de objeto genérico 'T'
+ */
+template<class T>
+void DoublyLinkedList<T>::setStepperToStart(bool b)
+{
+    if (b) stepper = header;
+    else stepper = tail;
+}
+
+/**
+ * Carrega o conteúdo apontado por stepper em
+ * uma variavel de referência outContent, se
+ * a lista estiver vazia, nenhum conteúdo é carregado,
+ * método retorna false indicando essa situação
+ * @param outContent conteúdo apontado por stepper
+ * @tparam T template de objeto genérico 'T'
+ * @return bool determina se outContent é coerente
+ */
+template<class T>
+bool DoublyLinkedList<T>::stepperContent(T &outContent)
+{
+    if (!header) return false;
+
+    outContent = stepper->getContent();
     return true;
 }
 
