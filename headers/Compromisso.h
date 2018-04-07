904d459313f41fc2148b9d746ac416a9acee0c3b
pedroschiavom <31569709+pedroschiavom@users.noreply.github.com>
2018-04-07 14:02:33 -0300

    Add files via upload

--- headers/Compromisso.h
+++ headers/Compromisso.h
@@ -1,27 +1,26 @@
-
-
 #ifndef COMPROMISSO_H
 #define COMPROMISSO_H
 
 #include "Data.h"
 #include "Horario.h"
+#include "json.h"
 #include <iostream>
 using namespace std;
 
 class Compromisso {
 
 public:
-    Compromisso();
+    Compromisso()=default;
     Compromisso(Data&, Horario&, const string &);
-    Compromisso(Data, Horario, const string &);
+    //Compromisso(Data, Horario, const string &);
     string getDescricao() const;
-    void setDescricao(string &);
+    void setDescricao(const string &);
     void setDescricao(const char[]);
     string getTitulo() const;
-    void setTitulo(string &);
+    void setTitulo(const string &);
     void setTitulo(const char[]);
     string getLocal() const;
-    void setLocal(string &);
+    void setLocal(const string &);
     void setLocal(const char[]);
 
     bool operator==(const Compromisso &);
@@ -31,8 +30,10 @@ public:
     bool operator<=(const Compromisso &);
     bool operator<(const Compromisso &);
     Compromisso& operator=(const Compromisso &);
-
     friend ostream& operator<<(ostream &, const Compromisso &);
+
+    Json::Value toJson();
+    void fromJson(Json::Value);
 
     Data data;
     Horario horario;
@@ -42,6 +43,5 @@ private:
     string local;
     string titulo;
 };
-
 
 #endif //COMPROMISSO_H
