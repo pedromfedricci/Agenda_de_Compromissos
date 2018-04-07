a0f851554deae95279548a9e174c0a5f121e2ff8
thyagomelo02 <31568098+thyagomelo02@users.noreply.github.com>
2018-04-07 18:59:14 -0300

    Add files via upload
    upando ao master

---
+++ src/compromissoswindow.cpp
@@ -0,0 +1,116 @@
+#include "headers/compromissoswindow.h"
+#include "ui_compromissoswindow.h"
+
+CompromissosWindow::CompromissosWindow(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::CompromissosWindow)
+{
+    ui->setupUi(this);
+}
+
+CompromissosWindow::CompromissosWindow(MyVector<Usuario> *usuarios, QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::CompromissosWindow)
+{
+    ui->setupUi(this);
+    this->setWindowTitle("COMPROMISSOS");
+
+    ptr_usuarios = usuarios;
+
+    QString label;
+    label.append(QString::fromLatin1("Ola "));
+    label.append(QString::fromStdString(Sessao::getUsername()));
+    ui->labelOla->setText(label);
+
+    posicao = 1;
+    carregarDaPosicao(posicao);
+}
+
+void CompromissosWindow::carregarDaPosicao(long pos)
+{
+    Compromisso temp;
+    bool valid = ptr_usuarios->contentAt(Sessao::getPosicao()).compromissos.iterate(temp ,pos);
+
+    if (valid)
+    {
+        ui->lineEditAno->setText(QString::number(temp.data.getAno()));
+        ui->lineEditMes->setText(QString::fromStdString(temp.data.getMesString()));
+        ui->lineEditDia->setText(QString::number(temp.data.getDia()));
+
+        ui->lineEditHora->setText(QString::number(temp.horario.getHora()));
+        ui->lineEditMinuto->setText(QString::number(temp.horario.getMinuto()));
+        ui->lineEditSegundo->setText(QString::number(temp.horario.getSegundo()));
+
+        ui->lineEditTitulo->setText(QString::fromStdString(temp.getTitulo()));
+        ui->lineEditLocal->setText(QString::fromStdString(temp.getLocal()));
+        ui->plainTextEditDescricao->document()->setPlainText(QString::fromStdString(temp.getDescricao()));
+    }
+
+    else resetLines();
+}
+
+long CompromissosWindow::andarParaFrente(bool b)
+{
+    long temp;
+    long size = ptr_usuarios->contentAt(Sessao::getPosicao()).compromissos.getSize();
+    if (b) temp = 1;
+    else temp = -1;
+
+    posicao = posicao + temp;
+    if (posicao > size) posicao = 1;
+    else if (posicao <= 0) posicao = size;
+
+    return posicao;
+}
+
+CompromissosWindow::~CompromissosWindow()
+{
+    delete ui;
+}
+
+void CompromissosWindow::on_pushButtonVoltar_clicked()
+{
+    this->parentWidget()->show();
+    this->close();
+}
+
+void CompromissosWindow::on_pushButtonSair_clicked()
+{
+    this->close();
+    qApp->quit();
+}
+
+void CompromissosWindow::on_pushButtonAnterior_clicked()
+{
+    carregarDaPosicao(andarParaFrente(false));
+}
+
+void CompromissosWindow::on_pushButtonProximo_clicked()
+{
+    carregarDaPosicao(andarParaFrente(true));
+}
+
+void CompromissosWindow::on_pushButtonRemover_clicked()
+{
+    Compromisso temp;
+
+    if (ptr_usuarios->contentAt(Sessao::getPosicao()).compromissos.iterate(temp, this->posicao))
+        ptr_usuarios->contentAt(Sessao::getPosicao()).compromissos.removeOne(temp);
+
+    CompromissosWindow::carregarDaPosicao(andarParaFrente(true));
+}
+
+void CompromissosWindow::resetLines()
+{
+    ui->lineEditAno->setText("");
+    ui->lineEditMes->setText("");
+    ui->lineEditDia->setText("");
+
+    ui->lineEditHora->setText("");
+    ui->lineEditMinuto->setText("");
+    ui->lineEditSegundo->setText("");
+
+    ui->lineEditTitulo->setText("");
+    ui->lineEditLocal->setText("");
+    ui->plainTextEditDescricao->document()->setPlainText("");
+}
---
+++ src/menuwindow.cpp
@@ -0,0 +1,85 @@
+#include "headers/menuwindow.h"
+#include "headers/perfilwindow.h"
+#include "headers/compromissoswindow.h"
+#include "ui_menuwindow.h"
+
+MenuWindow::MenuWindow(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::MenuWindow)
+{
+    ui->setupUi(this);
+}
+
+MenuWindow::MenuWindow(MyVector<Usuario> *usuarios, QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::MenuWindow)
+{
+    ui->setupUi(this);
+    this->setWindowTitle("MENU");
+
+    ptr_usuarios = usuarios;
+
+    QString label;
+    label.append(QString::fromStdString("Ola "));
+    label.append(QString::fromStdString(Sessao::getUsername()));
+    ui->labelOla->setText(label);
+}
+
+MenuWindow::~MenuWindow()
+{
+    delete ui;
+    Sessao::reset();
+}
+
+void MenuWindow::on_pushButtonSair_clicked()
+{
+    this->close();
+    qApp->quit();
+}
+
+void MenuWindow::on_pushButtonVoltar_clicked()
+{
+    this->parentWidget()->show();
+    this->close();
+}
+
+
+void MenuWindow::on_pushButtonPerfil_clicked()
+{
+    ptr_window = new PerfilWindow(ptr_usuarios,this);
+    ptr_window->setAttribute(Qt::WA_DeleteOnClose);
+    //connect(ptr_window, &QDialog::destroyed, this, &MenuWindow::show);
+
+    ptr_window->show();
+    this->hide();
+}
+
+void MenuWindow::on_pushButtonCompromissos_clicked()
+{
+    ptr_window = new CompromissosWindow(ptr_usuarios, this);
+    ptr_window->setAttribute(Qt::WA_DeleteOnClose);
+    //connect(ptr_window, &QDialog::destroyed, this, &MenuWindow::show);
+
+    ptr_window->show();
+    this->hide();
+}
+
+void MenuWindow::on_pushButtonNovo_clicked()
+{
+    ptr_window = new NovoCompromissoWindow(ptr_usuarios, this);
+    ptr_window->setAttribute(Qt::WA_DeleteOnClose);
+    //connect(ptr_window, &QDialog::destroyed, this, &MenuWindow::show);
+
+    ptr_window->show();
+    this->hide();
+}
+
+void MenuWindow::on_pushButtonRemover_clicked()
+{
+    Usuario usuario(Sessao::getUsername());
+    ptr_usuarios->removeOne(usuario);
+    Sessao::reset();
+
+    this->parentWidget()->show();
+    this->close();
+}
